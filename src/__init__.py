16135a4d67630b9c523378d7dacc863e8b7fccfc
Jacqueline <121686418+Jacquelinepersha@users.noreply.github.com>
2026-08-17 12:53:57 -0400

    Delete src directory

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-from .data_loader import load_unsw, load_feature_dictionary
-from .preprocessing import split_xy, build_preprocessor, align_columns
-from .models import logistic_pipeline, random_forest_pipeline, xgboost_pipeline
-from .evaluation import (
-    binary_metrics,
-    binary_metrics_frame,
-    multiclass_report,
-    error_rows,
-)
